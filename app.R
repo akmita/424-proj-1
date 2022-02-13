49377e8620154cba08df6893f3079a6eb98817e9
Adrian Kmita <kmita111@gmail.com>
2022-02-12 22:45:21 -0600

    stable version

--- app.R
+++ app.R
@@ -1,26 +1,294 @@
 library(shiny)
+library(ggplot2)
+library(lubridate)
 
+# global vars
+graphChoices <- c("Total Rides Yearly", "Total Rides Daily", "Total Rides Monthly", "Total Rides By Day of the Week")
+viewTypes <- c("bar", "table")
+
+
+#
 # add UI elements to the app
+#
 ui <- fluidPage(
-  sliderInput(inputId = "num",
-              label = "Choose a number",
-              value = 25, min = 1, max = 100),
-  plotOutput("hist")
+  column(6, 
+     column(4,sliderInput(
+       inputId = "UIC_year",
+        label = "Choose a year",
+        value = 2021, min = 2001, max = 2021, ticks = FALSE)
+     ),
+     column(4,radioButtons(
+       inputId = "UIC_graphType",
+       label = "Choose the bar graph to show",
+       choices = graphChoices)
+     ),
+     column(4,radioButtons(
+       inputId = "UIC_tableOrBar",
+       label = "Choose raw table view or bar graph",
+       choices = viewTypes)
+     ),
+     column(12, wellPanel(
+       conditionalPanel(
+         condition = "input.UIC_tableOrBar == 'bar'",
+         plotOutput("UICbar"),
+       ),
+       conditionalPanel(
+         condition = "input.UIC_tableOrBar == 'table'",
+         tableOutput("UICtable"),
+       ),
+     )
+   ),
+   style = "height: 90vh; overflow: scroll;"
+  ),
+  column(6, 
+   column(4,sliderInput(
+     inputId = "ohare_year",
+     label = "Choose a year",
+     value = 2021, min = 2001, max = 2021, ticks = FALSE)
+   ),
+   column(4,radioButtons(
+     inputId = "ohare_graphType",
+     label = "Choose the bar graph to show",
+     choices = graphChoices)
+   ),
+   column(4,radioButtons(
+     inputId = "ohare_tableOrBar",
+     label = "Choose raw table view or bar graph",
+     choices = viewTypes)
+   ),
+   column(12, wellPanel(
+     conditionalPanel(
+       condition = "input.ohare_tableOrBar == 'bar'",
+       plotOutput("oharebar"),
+     ),
+     conditionalPanel(
+       condition = "input.ohare_tableOrBar == 'table'",
+       tableOutput("oharetable"),
+     ),
+   )
+   ),
+   style = "height: 90vh; overflow: scroll;"   
+  ),
+  style="padding: 50px;"
 )
 
 
+#
 # send inputs into outputs
+#
 server <- function(input, output) {
   # both input and output must be in the server func args
   
-  # our plot named "hist" above
-  # save all objects you want to display inside output object
-  # everything saved into output needs a render function
-  output$hist <- renderPlot({ 
-    # can declare variables, treat it like an inline function
-    ggplot(data=UIC_agg, aes(x=year, y=rides)) + geom_bar(stat="identity") 
-  }) 
+  print(head(iris))
   
+  # UIC data
+  output$UICbar <- renderPlot({
+    createBarGraph("UIC-Halsted", input$UIC_graphType, input$UIC_year, D_main)  # TODO change to global dataset
+  })
+
+  # ohare data
+  output$oharebar <- renderPlot({
+    createBarGraph("ohare", input$ohare_graphType, input$ohare_year, D_main)  # TODO change to global dataset
+  })
+ 
+ 
+  output$UICtable <- renderTable(getTable(input$UIC_graphType, input$UIC_year, "UIC-Halsted", D_main))
+
+
+
+  output$oharetable <- renderTable(getTable(input$ohare_graphType, input$ohare_year, "O'hare", D_main))
+  
+}
+
+
+
+#
+# creates a bar graph based on user criteria 
+# 
+# @location - location of stations
+# @barGraphSelect - type of bar graph user selected
+# @yearSelected - year the user selected
+# @D - station and ride count dataset 
+# 
+createBarGraph <- function(location, barGraphSelect, yearSelected, D) {
+  print(head(D))
+  
+  # yearly view
+  if (barGraphSelect == graphChoices[1]) {
+      (ggplot(
+        data=parseByYear(D, location), 
+        aes(x=year, y=rides)) 
+       + geom_bar(stat="identity") 
+       + labs(title = paste(barGraphSelect, "at", location))
+      )
+  }
+  # daily view
+  else if (barGraphSelect == graphChoices[2]) {
+    (ggplot(
+      data=parseByDay(D, yearSelected, location), 
+      aes(x=newDate, y=rides))
+      + geom_bar(stat="identity")
+      + labs(title = paste(barGraphSelect, "at", location), x = "Date")
+    )
+  }
+  # monthly view
+  else if (barGraphSelect == graphChoices[3]) {
+    # TODO
+    (ggplot(data=parseByMonth(D, yearSelected, location), aes(x=month, y=rides))
+     + geom_bar(stat="identity")
+     + labs(title = paste(barGraphSelect, "at", location), x = "Month")
+    )
+  }
+  # day of the week view
+  else if (barGraphSelect == graphChoices[4]) {
+# TODO
+    
+    (ggplot(data=parseByWeekday(D, yearSelected, location), aes(x=dayOfWeek, y=rides))
+      + geom_bar(stat="identity")
+      + labs(title = paste(barGraphSelect, "at", location), x = "Day of the Week")
+    )
+  }
+  else {
+    print("failed to load graph")
+  }
+}
+
+
+
+#
+# get appropriate graph data for table
+#
+getTable = function(barGraphSelect, yearSelected, location, D) {
+  
+  print(paste(barGraphSelect, yearSelected, location))
+  
+  # yearly view
+  if (barGraphSelect == graphChoices[1]) {
+    return(parseByYear(D, location))
+  }
+  # daily view
+  else if (barGraphSelect == graphChoices[2]) {
+    return(parseByDay(D, yearSelected, location))
+  }
+  # monthly view  
+  else if (barGraphSelect == graphChoices[3]) {
+    return(parseByMonth(D, yearSelected, location))
+  }
+  # day of the week view
+  else if (barGraphSelect == graphChoices[4]) {
+    return(parseByWeekday(D, yearSelected, location))
+  }
+  else {
+    print("failed to load table")
+  }
+}
+
+
+#
+# parse dataset to show rides per year
+#
+parseByYear = function(D, location) {
+  D <- filterByLocation(D, location)
+  
+  D <- aggregate(rides~year,D,sum)        # group by year
+  return(D);
+}
+
+
+#
+# parse dataset to show rides per day, for specific year
+#
+parseByDay = function(D, yearSelected, location) {
+  D <- filterByLocation(D, location)
+  D <- subset(D, format(D$newDate, format="%Y") == yearSelected) # filter by year
+  return(D)
+}
+
+
+#
+# parse dataset to show rides per month, specific year
+#
+parseByMonth = function(D, yearSelected, location) {
+  D <- filterByLocation(D, location)
+  D <- subset(D, format(D$newDate, format="%Y") == yearSelected) # get subset only selected year
+  D <- aggregate(rides~month,D,sum) # aggregate per month
+  return(D)
+}
+
+
+#
+#  parse dataset to show rides per day of week, given year
+#
+parseByWeekday = function(D, yearSelected, location) {
+  D <- filterByLocation(D, location)
+  D <- subset(D, format(D$newDate, format="%Y") == yearSelected)  # get subset only selected year
+  D$dayOfWeek <- weekdays(as.Date(D$newDate))   # get weekdays 
+  D <- aggregate(rides~dayOfWeek,D,sum)         # group by weekday
+  # D <- D[c(4,5,6,7,1,2,3),] # try to rearrange days in better order
+  return(D)
+}
+
+
+filterByLocation = function(D, location) {
+  print(head(D))
+  
+  if (location == "UIC-Halsted") {
+    D <- subset(D, stationname == "UIC-Halsted") # filter only selected station
+  }
+  else if (grepl( "o'hare", location, fixed = TRUE)) {
+    D <- subset(D, 
+                (stationname == "Irving Park-O'Hare" 
+                | stationname ==  "Harlem-O'Hare" 
+                | stationname ==  "Addison-O'Hare" 
+                | stationname ==  "O'Hare Airport" 
+                | stationname ==  "Montrose-O'Hare" 
+                | stationname ==  "Belmont-O'Hare"))
+  }
+
+  return(D)
+    
+}
+
+
+
+# 
+#  reads data from already split tsv file and formats date using lubridate
+# 
+readAndParseData = function() {
+  # load first frame
+  DF <- read.table(
+    file = "CTA_-_Ridership_-__L__Station_Entries_-_Daily_Totals_1.tsv", 
+    sep = "\t", header = TRUE)
+  names(DF)<-c("station_id","stationname", "date", "daytype", "rides")
+  
+  # load and concatenate following frames
+  i <- 2
+  while (i <= 11) {
+    DF <- rbind(DF, read.table(
+      file = paste0("CTA_-_Ridership_-__L__Station_Entries_-_Daily_Totals_", i, ".tsv"), 
+      sep = "\t", header = TRUE)
+    )
+    i <- i+1
+  }
+
+  # convert dates to newDate
+  DF$date <- mdy(DF$date)
+  DF$newDate <- as.Date(DF$date, "%Y/%m/%d")
+  
+  # split date into components
+  DF$year <- format(DF$newDate, format="%Y")
+  DF$month <- format(DF$newDate, format="%m")
+  DF$day <- format(DF$newDate, format="%d")
+  
+  return(DF)
+}
+
+
+#
+# main dataset, reformatted dates
+#
+if (!exists("D_main",  mode="environment")) {
+  D_main <- readAndParseData()
 }
 
 shinyApp(ui = ui, server = server)
--- project _1.R
+++ project _1.R
@@ -12,7 +12,10 @@ d8 <- read.table(file = "CTA_-_Ridership_-__L__Station_Entries_-_Daily_Totals_8.
 d9 <- read.table(file = "CTA_-_Ridership_-__L__Station_Entries_-_Daily_Totals_9.tsv", sep = "\t", header = TRUE)
 d10 <- read.table(file = "CTA_-_Ridership_-__L__Station_Entries_-_Daily_Totals_10.tsv", sep = "\t", header = TRUE)
 d11 <- read.table(file = "CTA_-_Ridership_-__L__Station_Entries_-_Daily_Totals_11.tsv", sep = "\t", header = TRUE)
-				
+			
+
+
+	
 # 2. fix messed up coluymn names
 names(d1)<-c("station_id","stationname", "date", "daytype", "rides")
 
@@ -22,7 +25,6 @@ D <- rbind(D, d11)
 # 4. convert dates
 library(lubridate)
 D_date_formatted <- D
-D_date_formatted$date <- as.Date(D$date, '%m-%d-%y')
 D_date_formatted$date <- mdy(D$date)
 
 # 5. get only UIC data
@@ -32,11 +34,14 @@ D_UIC <- subset(D_date_formatted, station_id == 40350)
 D_UIC$year <- format(D_UIC$date, format="%Y")
 D_UIC$month <- format(D_UIC$date, format="%m")
 D_UIC$day <- format(D_UIC$date, format="%d")
+D_UIC$newDate <- as.Date(D_UIC$date, "%Y/%m/%d")
 
 # 7. aggregate and count
 # library(sqldf)
 # test <- sqldf("SELECT COUNT(*) FROM D_UIC AS count GROUP BY years")
+# by year
 UIC_agg <- aggregate(rides~year,D_UIC,sum)
+# daily, we don't need agregationm, we need to elimitate anythign but years
 
 
 
@@ -46,6 +51,14 @@ UIC_agg <- aggregate(rides~year,D_UIC,sum)
 library(ggplot2)
 ggplot(data=UIC_agg, aes(x=year, y=rides)) + geom_bar(stat="identity") 
 
+ggplot(data=subset(D_UIC, year == 2021), aes(x=newDate, y=rides)) + geom_bar(stat="identity") 
+
+test <- subset(D_UIC, year == 2021)
+test$dayOfWeek <- weekdays(as.Date(test$newDate))
+test <- aggregate(rides~dayOfWeek,test,sum)
+
+
+ggplot()
 
 
 save.image("~/FALL 2022 offline/CS 424 offline/project 1/proj_1_workspace.RData")
@@ -69,4 +82,60 @@ typeof(D_UIC$years)
 data("midwest", package = "ggplot2")
 ggplot(midwest, aes(x=area, y=poptotal)) + geom_point()
 
+test <- subset(D, stationname = "")
 
+
+
+# 
+#  reads data from already split tsv file and formats date using lubridate
+# 
+readAndParseData = function() {
+  # load first frame
+  DF <- read.table(
+    file = "CTA_-_Ridership_-__L__Station_Entries_-_Daily_Totals_1.tsv", 
+    sep = "\t", header = TRUE)
+  names(DF)<-c("station_id","stationname", "date", "daytype", "rides")
+  
+  # load and concatenate following frames
+  i <- 2
+  while (i <= 11) {
+    DF <- rbind(DF, read.table(
+      file = paste0("CTA_-_Ridership_-__L__Station_Entries_-_Daily_Totals_", i, ".tsv"), 
+      sep = "\t", header = TRUE)
+    )
+    i <- i+1
+  }
+  
+  # convert dates to newDate
+  DF$date <- mdy(DF$date)
+  DF$newDate <- as.Date(DF$date, "%Y/%m/%d")
+  
+  # split date into components
+  DF$year <- format(DF$newDate, format="%Y")
+  DF$month <- format(DF$newDate, format="%m")
+  DF$day <- format(DF$newDate, format="%d")
+  
+  return(DF)
+}
+
+
+#
+# main dataset, reformatted dates
+#
+D_main <- readAndParseData()
+
+
+
+test <- subset(D_main, any(stationname == "ohare", "o'hare")) # filter only selected station
+
+
+test <- grepl("o'hare", D_main$stationname)
+
+
+test <- grepl("o'hare", D_main$stationname)
+
+
+
+D_ <- subset(D, any(grepl("o'hare", D_main$stationname, fixed = TRUE))
+
+             
